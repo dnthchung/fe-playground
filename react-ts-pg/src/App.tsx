54558a9f05e680d7a18ddac14683c0b78bf795e9
dnthchung <doanthanhchung03nb@gmail.com>
2024-11-16 23:31:19 +0700

    feat: add Button component and update TypeScript configuration; include @types/node and alias setup

--- react-ts-pg/src/App.tsx
+++ react-ts-pg/src/App.tsx
@@ -1,7 +1,11 @@
+import Button from "@/components/button";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-500">
-      Tailwind CSS is working!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind CSS is working!
+      </h1>
+      <Button />
+    </>
   );
 }
--- react-ts-pg/vite.config.ts
+++ react-ts-pg/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//path : vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Alias "@" trỏ tới thư mục "src"
+    },
+  },
+});
