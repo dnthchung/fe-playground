6acbbf59ce7dc43a1a3695213ecb4a344108a2dc
dnthchung <doanthanhchung03nb@gmail.com>
2024-11-17 00:39:53 +0700

    feat: update App component layout and enhance Button component with customizable props

--- react-ts-pg/src/App.tsx
+++ react-ts-pg/src/App.tsx
@@ -2,10 +2,16 @@ import Button from "@/components/button";
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-500">
-        Tailwind CSS is working!
-      </h1>
-      <Button />
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <Button
+          backgroundColor="bg-red-500 hover:bg-red-700"
+          text="Button"
+          fontSize={20}
+          pillShape={true}
+        />
+      </div>
     </>
   );
 }
+
+// React.FC chỉ dùng cho tằng const Component: React.FC<Props> = (props) => {}
--- react-ts-pg/src/components/button.tsx
+++ react-ts-pg/src/components/button.tsx
@@ -1,5 +1,57 @@
 import React from "react";
 
-export default function Button() {
-  return <div>button</div>;
+// export default function Button(propNe: {
+//   backgroundColor: string;
+//   text: string;
+// }) {
+//   const { backgroundColor, text } = propNe;
+//   return (
+//     <div>
+//       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+//         Button
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default function Button({
+//   backgroundColor,
+//   text,
+//   fontSize,
+//   pillShape,
+// }: {
+//   backgroundColor: string;
+//   text: string;
+//   fontSize: number;
+//   pillShape?: boolean;
+// }) {
+//   return (
+//     <div>
+//       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+//         Button
+//       </button>
+//     </div>
+//   );
+// }
+
+//c3
+type ButtonProps = {
+  backgroundColor: string; //property
+  text: string;
+  fontSize: number;
+  pillShape: boolean;
+};
+export default function Button({
+  backgroundColor,
+  text,
+  fontSize,
+  pillShape,
+}: ButtonProps) {
+  return (
+    <div>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </div>
+  );
 }
