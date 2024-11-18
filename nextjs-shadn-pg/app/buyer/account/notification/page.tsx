779983284f35bb580f2193fa32c419eddfed50ab
dnthchung <doanthanhchung03nb@gmail.com>
2024-11-18 07:43:18 +0700

    feat: add initial page components and layout for buyer account section

---
+++ nextjs-shadn-pg/app/buyer/account/notification/page.tsx
@@ -0,0 +1,7 @@
+// notification settings page
+
+import React from "react";
+
+export default function Notification() {
+  return <div> Notification</div>;
+}
---
+++ nextjs-shadn-pg/app/buyer/account/page.tsx
@@ -0,0 +1,7 @@
+//account page dad
+
+import React from "react";
+
+export default function Account() {
+  return <div> Account dad</div>;
+}
