f1230e7c06dc8c89f8ff7dd6cece1fbcf56233ee
dnthchung <doanthanhchung03nb@gmail.com>
2024-11-20 00:25:45 +0700

    refactor: remove deprecated theme mode toggle component; update exports in index file

--- nextjs-shadcn-base/src/components/index.ts
+++ nextjs-shadcn-base/src/components/index.ts
@@ -1,5 +1,4 @@
 //Features
-export { ModeToggle } from "@/components/features/theme-mode-toggle2";
 export * from "@/components/features/theme-mode-toggle";
 export * from "@/components/features/theme-color-toggle";
 
