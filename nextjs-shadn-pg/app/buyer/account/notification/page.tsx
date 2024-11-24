ac88608db32a6947b32235db451ef83ae7c0aac9
dnthchung <doanthanhchung03nb@gmail.com>
2024-11-24 14:37:19 +0700

    mdf

--- nextjs-shadn-pg/app/buyer/account/notification/page.tsx
+++
@@ -1,7 +0,0 @@
-// notification settings page
-
-import React from "react";
-
-export default function Notification() {
-  return <div> Notification</div>;
-}
--- nextjs-shadn-pg/app/buyer/account/page.tsx
+++
@@ -1,7 +0,0 @@
-//account page dad
-
-import React from "react";
-
-export default function Account() {
-  return <div> Account dad</div>;
-}
