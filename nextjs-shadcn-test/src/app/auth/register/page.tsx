c592c0fb404c665f648c2b9800095c6c90c9424f
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-02 07:33:52 +0700

    feat: add initial setup for Next.js with Tailwind CSS; implement authentication components and utility functions

---
+++ nextjs-shadcn-test/src/app/auth/register/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+
+import { Button, Label, Input } from "@/components";
+import { EyeClosed as EyeClosedIcon, Eye as EyeOpenIcon } from "lucide-react";
+
+export default function Register() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Register</h1>
+            <p className="text-balance text-muted-foreground">Create your account by filling in the details below</p>
+          </div>
+          <div className="grid gap-4">
+            {/* Full Name Field */}
+            <div className="grid gap-2">
+              <Label htmlFor="fullname">Full Name</Label>
+              <Input id="fullname" type="text" placeholder="John Doe" required />
+            </div>
+            {/* Email Field */}
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="johndoe@example.com" required />
+            </div>
+            {/* Password Field */}
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <div className="relative">
+                <Input id="password" type={passwordVisible ? "text" : "password"} placeholder="Enter your password" required />
+                <button type="button" className="absolute inset-y-0 right-2 flex items-center" onClick={() => setPasswordVisible(!passwordVisible)}>
+                  {passwordVisible ? <EyeOpenIcon strokeWidth={1.5} size={18} /> : <EyeClosedIcon strokeWidth={1.5} size={18} />}
+                </button>
+              </div>
+            </div>
+            {/* Confirm Password Field */}
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="confirm-password">Confirm Password</Label>
+              </div>
+              <div className="relative">
+                <Input id="confirm-password" type={confirmPasswordVisible ? "text" : "password"} placeholder="Confirm your password" required />
+                <button type="button" className="absolute inset-y-0 right-2 flex items-center" onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}>
+                  {confirmPasswordVisible ? <EyeOpenIcon strokeWidth={1.5} size={18} /> : <EyeClosedIcon strokeWidth={1.5} size={18} />}
+                </button>
+              </div>
+            </div>
+            {/* Register Button */}
+            <Button type="submit" className="w-full">
+              Register
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/auth/login" className="underline">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">{/* You can add a cover image here */}</div>
+    </div>
+  );
+}
---
+++ nextjs-shadcn-test/src/components/index.ts
@@ -0,0 +1,30 @@
+//Features
+export * from "@/components/features/theme-mode-toggle";
+export * from "@/components/features/theme-color-toggle";
+
+//Widget - loading ...
+
+//shadcn - Ui
+export { Button } from "./shadcn/ui/button";
+export { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./shadcn/ui/dropdown-menu";
+export { Input } from "./shadcn/ui/input";
+export { Label } from "./shadcn/ui/label";
+export * from "./shadcn/ui/hover-card";
+export * from "./shadcn/ui/form";
+export * from "./shadcn/ui/checkbox";
+export * from "./shadcn/ui/radio-group";
+export * from "./shadcn/ui/select";
+export * from "./shadcn/ui/tabs";
+export * from "./shadcn/ui/tooltip";
+export * from "./shadcn/ui/switch";
+export * from "./shadcn/ui/textarea";
+export * from "./shadcn/ui/progress";
+export * from "./shadcn/ui/avatar";
+
+// ============================== Thay vì ==============================
+// import { Button } from "@/components/shadcn/ui/button";
+// import { Input } from "@/components/shadcn/ui/input";
+// import { Label } from "@/components/shadcn/ui/label";
+
+// ============================== Bây giờ ==============================
+// import { Button, Input, Label } from "@/components";
