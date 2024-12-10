00c470c9d80020a0f047eb0bdd74a02f83621183
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-10 21:28:54 +0700

    feat: implement new UI components including AspectRatio, Skeleton, Collapsible, Textarea, Label, Input, Progress, Separator, and Toaster; remove deprecated UI files

--- nextjs-shadcn-test/src/components/index.ts
+++ nextjs-shadcn-test/src/components/index.ts
@@ -5,21 +5,21 @@ export * from "@/components/features/theme-color-toggle";
 //Widget - loading ...
 
 //shadcn - Ui
-export { Button } from "./shadcn/ui/button";
-export { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./shadcn/ui/dropdown-menu";
-export { Input } from "./shadcn/ui/input";
-export { Label } from "./shadcn/ui/label";
-export * from "./shadcn/ui/hover-card";
-export * from "./shadcn/ui/form";
-export * from "./shadcn/ui/checkbox";
-export * from "./shadcn/ui/radio-group";
-export * from "./shadcn/ui/select";
-export * from "./shadcn/ui/tabs";
-export * from "./shadcn/ui/tooltip";
-export * from "./shadcn/ui/switch";
-export * from "./shadcn/ui/textarea";
-export * from "./shadcn/ui/progress";
-export * from "./shadcn/ui/avatar";
+export { Button } from "./shadcn/button";
+export { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./shadcn/dropdown-menu";
+export { Input } from "./shadcn/input";
+export { Label } from "./shadcn/label";
+export * from "./shadcn/hover-card";
+export * from "./shadcn/form";
+export * from "./shadcn/checkbox";
+export * from "./shadcn/radio-group";
+export * from "./shadcn/select";
+export * from "./shadcn/tabs";
+export * from "./shadcn/tooltip";
+export * from "./shadcn/switch";
+export * from "./shadcn/textarea";
+export * from "./shadcn/progress";
+export * from "./shadcn/avatar";
 
 // ============================== Thay vì ==============================
 // import { Button } from "@/components/shadcn/ui/button";
