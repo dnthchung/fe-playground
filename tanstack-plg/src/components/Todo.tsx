f55a6f87d5ef0d6d5c33bd0749d26bfa89084ad9
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-22 13:22:21 +0700

    useIsFetching Hook

--- tanstack-plg/src/components/Todo.tsx
+++ tanstack-plg/src/components/Todo.tsx
@@ -1,14 +1,24 @@
+// import { useIsFetching } from "@tanstack/react-query";
 import { useTodosIds } from "../services/queries";
 
 export default function Todo() {
   const todosIdsQuery = useTodosIds();
-  if (todosIdsQuery.isLoading) return <div>Loading...</div>;
+  //   const isFetching = useIsFetching(); //Trả về số lượng query đang fetch.
+
+  if (todosIdsQuery.isLoading) return <div>Loading ne...</div>;
   if (todosIdsQuery.isError) return <div>Error ...</div>;
   return (
     <div>
       <h1>Todos</h1>
+      {/* <p>Query status - about data: {todosIdsQuery.status}</p>
+      <p>Fetch status - about calling BE process: {todosIdsQuery.fetchStatus}</p>
+      <p>Global fetching status: {isFetching}</p> */}
       {/* code 2 thêm code check data có tồn tại hay không vì có thể data chưa được fetch về*/}
-      <>{todosIdsQuery.data && todosIdsQuery.data.map((id) => <p key={id}>{id}</p>)}</>
+      <>
+        {todosIdsQuery.data?.map((id) => (
+          <p key={id}>{id}</p>
+        ))}
+      </>
     </div>
   );
 }
