ae1328ec3c93a9c0dd0cb12121c9fc1c7dcf44b2
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-22 11:42:11 +0700

    Wrap Your Project Inside React Query Client

--- tanstack-plg/src/App.tsx
+++ tanstack-plg/src/App.tsx
@@ -1,5 +1,11 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <>hi hi</>;
+  return (
+    <>
+      <Todo />
+    </>
+  );
 }
 
 export default App;
--- tanstack-plg/src/components/Todo.tsx
+++ tanstack-plg/src/components/Todo.tsx
@@ -1,1 +1,14 @@
-export default function Todo() {}
+import { useTodosIds } from "../services/queries";
+
+export default function Todo() {
+  const todosIdsQuery = useTodosIds();
+  if (todosIdsQuery.isLoading) return <div>Loading...</div>;
+  if (todosIdsQuery.isError) return <div>Error ...</div>;
+  return (
+    <div>
+      <h1>Todos</h1>
+      {/* code 2 thêm code check data có tồn tại hay không vì có thể data chưa được fetch về*/}
+      <>{todosIdsQuery.data && todosIdsQuery.data.map((id) => <p key={id}>{id}</p>)}</>
+    </div>
+  );
+}
