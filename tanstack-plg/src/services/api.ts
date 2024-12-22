fe65339d33a7224338a3c1173e79be4ec78341b7
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-22 14:43:06 +0700

    Parallel Queries

--- tanstack-plg/src/services/api.ts
+++ tanstack-plg/src/services/api.ts
@@ -8,6 +8,6 @@ export const getTodosIds = async () => {
   return (await axiosInstance.get<Todo[]>("todos")).data.map((todo) => todo.id);
 };
 
-export const getTodos = async () => {
-  return (await axiosInstance.get<Todo[]>( 
-}
+export const getTodo = async (id: number) => {
+  return (await axiosInstance.get<Todo>(`todo/${id}`)).data;
+};
--- tanstack-plg/src/services/queries.ts
+++ tanstack-plg/src/services/queries.ts
@@ -1,9 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTodosIds } from "./api";
+import { useQuery, useQueries } from "@tanstack/react-query";
+import { getTodo, getTodosIds } from "./api";
 
 export function useTodosIds() {
   return useQuery({
     queryKey: ["todos"], // Đây là key để xác định duy nhất query này
     queryFn: getTodosIds, // Hàm để fetch dữ liệu
+  });
+}
+
+export function useTodos(ids: (number | undefined)[] | undefined) {
+  return useQueries({
+    // Dùng useQueries Fetch dữ liệu cho nhiều API endpoint cùng lúc.
+    queries: (ids ?? []).map((id) => {
+      return {
+        queryKey: ["todo", id],
+        queryFn: () => getTodo(id!),
+      };
+    }),
   });
 }
