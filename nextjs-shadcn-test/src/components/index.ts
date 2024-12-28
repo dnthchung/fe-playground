c28d851d6422d1e4462731339655fb07dd104f45
dnthchung <doanthanhchung03nb@gmail.com>
2024-12-28 22:15:29 +0700

    Refactor exports and simplify component definitions in Avatar and Toast hooks

--- nextjs-shadcn-test/src/components/index.ts
+++ nextjs-shadcn-test/src/components/index.ts
@@ -1,26 +1,27 @@
 //Features
-export * from "@/components/features/theme-mode-toggle";
-export * from "@/components/features/theme-color-toggle";
+export * from "@/components/features/theme-mode-toggle"
+export * from "@/components/features/theme-color-toggle"
 
 //Widget - loading ...
 
 //shadcn - Ui
-export { Button } from "./shadcn/button";
-export { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./shadcn/dropdown-menu";
-export { Input } from "./shadcn/input";
-export { Label } from "./shadcn/label";
-export * from "./shadcn/hover-card";
-export * from "./shadcn/form";
-export * from "./shadcn/checkbox";
-export * from "./shadcn/radio-group";
-export * from "./shadcn/select";
-export * from "./shadcn/tabs";
-export * from "./shadcn/tooltip";
-export * from "./shadcn/switch";
-export * from "./shadcn/textarea";
-export * from "./shadcn/progress";
-export * from "./shadcn/avatar";
-export * from "./shadcn/card";
+export { Button } from "./shadcn/button"
+export { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./shadcn/dropdown-menu"
+export { Input } from "./shadcn/input"
+export { Label } from "./shadcn/label"
+export * from "./shadcn/hover-card"
+export * from "./shadcn/form"
+export * from "./shadcn/checkbox"
+export * from "./shadcn/radio-group"
+export * from "./shadcn/select"
+export * from "./shadcn/tabs"
+export * from "./shadcn/tooltip"
+export * from "./shadcn/switch"
+export * from "./shadcn/textarea"
+export * from "./shadcn/progress"
+export * from "./shadcn/avatar"
+export * from "./shadcn/card"
+export * from "./shadcn/toast"
 
 // ============================== Thay vì ==============================
 // import { Button } from "@/components/shadcn/ui/button";
