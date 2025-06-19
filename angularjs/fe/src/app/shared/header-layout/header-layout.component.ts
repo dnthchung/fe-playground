e7a1cc6bf92045a7d564c02eabb845a633e30796
dnthchung <chungdthe176077@fpt.edu.vn>
2025-06-19 10:33:58 +0700

    mdf

---
+++ angularjs/fe/src/app/shared/header-layout/header-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-layout',
+  standalone: true,
+  templateUrl: './header-layout.component.html',
+  styleUrls: ['./header-layout.component.scss'],
+})
+export class HeaderLayoutComponent {}
