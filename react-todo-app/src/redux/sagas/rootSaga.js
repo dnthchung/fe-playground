2611f9caea84223d6c091c861d11bc0cbd88fb18
dnthchung <chungdthe176077@fpt.edu.vn>
2025-06-25 09:13:14 +0700

    mdf

--- react-todo-app/src/redux/sagas/rootSaga.js
+++
@@ -1,7 +0,0 @@
-// redux/sagas/rootSaga.js
-import { all } from "redux-saga/effects"
-import { todoWatcherSaga } from "./todoSaga"
-
-export default function* rootSaga() {
-  yield all([todoWatcherSaga()])
-}
