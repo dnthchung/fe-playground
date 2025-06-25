8178f060ab266b016c019479f3696085a9349b0b
dnthchung <chungdthe176077@fpt.edu.vn>
2025-06-25 09:56:37 +0700

    init

---
+++ angular-product/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Quét tất cả các file .html và .ts trong thư mục src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
